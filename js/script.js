61d823448a83ee259372a89b77ba6ef7c78c4053
arkawedspayel <149167864+arkawedspayel@users.noreply.github.com>
2023-11-21 06:49:53 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -51,8 +51,9 @@ var x = setInterval(function() {
     }
 }, 1000);
 setTimeout(function() {
-    var audio = document.getElementById("my_audio");
-    audio.play();
+    /*var audio = document.getElementById("my_audio");
+    audio.play();*/
+    $("body").click();
 
 }, 1000);
 
